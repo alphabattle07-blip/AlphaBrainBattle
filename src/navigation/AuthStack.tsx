cd72b61c1beb7affc616dd01741ef0705388eaf9
Jeremiah <jeremiah>
2025-12-16 01:05:50 +0100

    last-chances

--- src/navigation/AuthStack.tsx
+++ src/navigation/AuthStack.tsx
@@ -1,8 +1,8 @@
 // src/navigation/AuthStack.tsx
 import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import SignInScreen from '@/app/(auth)/SignInScreen';
-import SignUpScreen from '@/app/(auth)/SignUpScreen';
+import SignInScreen from '../../app/(auth)/SignInScreen';
+import SignUpScreen from '../../app/(auth)/SignUpScreen';
 import { RootStackParamList } from './types'; // Use your existing types
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
